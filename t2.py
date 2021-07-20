836b3630a02cbd3180a69e9c6cb7985cda40390c
felipeymn <felipe.ymn@gmail.com>
2021-07-20 09:51:14 -0300

    level order traversal implementation

--- t2.py
+++ t2.py
@@ -2,16 +2,30 @@ import sys
 
 
 class Node(object):
-    def __init__(self, name='root', children=None):
+    def __init__(self, name=0, parent=None, is_root=False):
         self.name = name
-        self.children = []
-        if children is not None:
-            for child in children:
-                self.add_child(child)
+        self.parent = parent
+        self.is_root = is_root
+        self.traveled_path = []
 
-    def add_child(self, node):
-        assert isinstance(node, Node)
-        self.children.append(node)
+    def __repr__(self):
+        return f'{self.name}'
+
+    def travel(self):
+        self.traveled_path.extend(self.parent.traveled_path)
+        if not self.parent.is_root:
+            self.traveled_path.append(self.parent)
+
+    def available_neighbours(self, paths):
+        neighbour = []
+        if (self.name == 0) and (self.is_root == False):
+            return neighbour
+        for i, path in enumerate(paths[self.name]):
+            if path:
+                names = [node.name for node in self.traveled_path]
+                if i not in names:
+                    neighbour.append(Node(name=i, parent=self))
+        return neighbour
 
 
 def read_input():
@@ -31,12 +45,31 @@ def upperbound(matrix):
     return 0
 
 
+def print_level_order(root, matrix):
+    if root is None:
+        return
+    q = []
+    q.append(root)
+    while q:
+        count = len(q)
+
+        while count > 0:
+            temp = q.pop(0)
+            print(f'{temp.name}', end=' ')
+            for neighbour in temp.available_neighbours(matrix):
+                neighbour.travel()
+                q.append(neighbour)
+
+            count -= 1
+        print(' ')
+
+
 def create_matrix(data, size):
 
     # set all lines to the same size
     for links in data:
         for _ in range(size - len(links)):
-            links.insert(0, '0')
+            links.insert(0, 0)
     data.append([0] * size)  # append the last line, which didnt come with the input data
 
     # copy the values above main diagonal to create a symmetric matrix
@@ -59,9 +92,10 @@ def main():
         sys.stderr.write(f'Error: {e}\n')
 
     matrix = create_matrix(links, size)
+    print('Matrix:')
     print_matrix(matrix)
-
-    # t = Node('*', [Node('1'), Node('2'), Node('+', [Node('3'), Node('4')])])
+    print('\nLevel Order Traversal:')
+    print_level_order(Node(is_root=True), matrix)
 
 
 if __name__ == '__main__':
